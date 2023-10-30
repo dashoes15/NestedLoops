dd991b1b5c5f6112fccbd9f5e3c64f323b216a8f
dashoes15 <daltonasali@gmail.com>
2023-10-30 09:21:27 -0400

    lab 7 task 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for (int i = 6; i>1; i--) {
+            for (int j = i; j<6; j++) {
+                System.out.print("*");
+            }
+            System.out.println("*");
+        }
+        System.out.println();
+        for (int i = 2; i<7; i++) {
+            for (int j = i; j<6; j++) {
+                System.out.print("*");
+            }
+            System.out.println("*");
+        }
+        System.out.println();
+        for (int i = 2; i<7; i++) {
+            for (int j = 1; j<5; j++) {
+                System.out.print("*");
+            }
+            System.out.println("*");
+        }
     }
 }
